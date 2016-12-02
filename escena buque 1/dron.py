098c1bca8a49f3fbf3b56886fca5729f5fb9571f
ccerezo <ccerezo90@gmail.com>
2016-12-02 00:14:51 -0500

    Se mueve el dron desde python

--- escena buque 1/dron.py
+++ escena buque 1/dron.py
@@ -1,6 +1,13 @@
 
 import vrep
 import sys
+import math
+import time
+
+posZ = 2
+x=0
+y=0
+ts=0.025
 vrep.simxFinish(-1)
 
 clientID=vrep.simxStart('127.0.0.1',19997,True,True,5000,5) # Connect to V-REP
@@ -11,7 +18,9 @@ else:
     print('Conexion Fallida')
     sys.exit('No se pudo Conectar')
 
-erroCode,motor_left = vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_leftMotor',vrep.simx_opmode_oneshot_wait)
-erroCode,motor_right = vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_rightMotor',vrep.simx_opmode_oneshot_wait)
-
-vrep.simxSetJointTargetVelocity(clientID,motor_left,0.3,vrep.simx_opmode_streaming)
+erroCode,dron = vrep.simxGetObjectHandle(clientID,'Quadricopter_target',vrep.simx_opmode_oneshot_wait)
+while (x>-1.27):
+    y=-1.33*x
+    x=x-0.0075
+    vrep.simxSetObjectPosition(clientID,dron,-1,(x,y,posZ),vrep.simx_opmode_oneshot)
+    time.sleep(ts)
