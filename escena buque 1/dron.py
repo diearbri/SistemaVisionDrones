13f3389d285e38164c473df7151bb09210e49a72
ccerezo <ccerezo90@gmail.com>
2016-12-09 01:11:59 -0500

    Primera Interfaz
    Botones para mover dron

--- escena buque 1/dron.py
+++ escena buque 1/dron.py
@@ -1,10 +1,10 @@
-
+from Tkinter import *
 import vrep
 import sys
 import math
 import time
 
-posZ = 2
+posZ = 0.5
 x=0
 y=0
 ts=0.025
@@ -19,8 +19,56 @@ else:
     sys.exit('No se pudo Conectar')
 
 erroCode,dron = vrep.simxGetObjectHandle(clientID,'Quadricopter_target',vrep.simx_opmode_oneshot_wait)
-while (x>-1.27):
-    y=-1.33*x
-    x=x-0.0075
-    vrep.simxSetObjectPosition(clientID,dron,-1,(x,y,posZ),vrep.simx_opmode_oneshot)
-    time.sleep(ts)
+##erroCode,camara = vrep.simxGetObjectHandle(clientID,'Vision_sensor',vrep.simx_opmode_oneshot_wait)
+##_,resolution,image = vrep.simxGetVisionSensorImage(clientID,camara,0,vrep.simx_opmode_streaming)
+##time.sleep(1)
+#########
+def moverY():
+    x=0
+    y=0
+    posZ=0.5
+    while (x>-1.5):
+        y=-1.33*x
+        x=x-0.0075
+        vrep.simxSetObjectPosition(clientID,dron,-1,(0,y,posZ),vrep.simx_opmode_oneshot)
+        time.sleep(ts)
+
+def moverX():
+    x=0
+    y=0
+    posZ=0.5
+    while (x>-1.5):
+        y=-1.33*x
+        x=x-0.0075
+        vrep.simxSetObjectPosition(clientID,dron,-1,(x,0,posZ),vrep.simx_opmode_oneshot)
+        time.sleep(ts)
+
+def moverZ():
+    x=0
+    z=0.5
+    while (z<2):
+        z=z+0.005
+        vrep.simxSetObjectPosition(clientID,dron,-1,(x,0,z),vrep.simx_opmode_oneshot)
+        time.sleep(ts)
+
+def menu():
+    op=0
+    while op!=1:
+        print("1.Salir")
+        print("2. Mover Y")
+        print("3. Mover X")
+        op = int(input('Ingrese Opcion:'))
+        if(op==2):
+            moverY()
+        if(op==3):
+            moverX()
+
+##menu()
+ventana = Frame(height=250,width=400)
+ventana.pack(padx=10,pady=10)
+etiqueta = Label(text="SIMULADOR DRON",font=("Verdana",18)).place(x=100,y=0)
+
+botonX = Button(ventana,command=moverY,text="<==").place(x=120,y=100)
+botonY = Button(ventana,command=moverX,text="==>").place(x=200,y=100)
+botonZ = Button(ventana,command=moverZ,text="Eje Z").place(x=160,y=65)
+ventana.mainloop()
