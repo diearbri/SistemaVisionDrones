c46b151046b07517da3ea45a16aa5946ad24265b
Diego Briones <diearbri@gmail.com>
2016-12-09 02:27:38 -0500

    se agrego zoom

--- escena buque 1/dron.py
+++ escena buque 1/dron.py
@@ -4,10 +4,11 @@ import sys
 import math
 import time
 
-posZ = 0.5
-x=0
-y=0
-ts=0.025
+posZ = 0.5100
+x=-3.7750
+y=-4.6250
+ts=0.5
+maxDesplamiento = 5;
 vrep.simxFinish(-1)
 
 clientID=vrep.simxStart('127.0.0.1',19997,True,True,5000,5) # Connect to V-REP
@@ -19,36 +20,75 @@ else:
     sys.exit('No se pudo Conectar')
 
 erroCode,dron = vrep.simxGetObjectHandle(clientID,'Quadricopter_target',vrep.simx_opmode_oneshot_wait)
-##erroCode,camara = vrep.simxGetObjectHandle(clientID,'Vision_sensor',vrep.simx_opmode_oneshot_wait)
-##_,resolution,image = vrep.simxGetVisionSensorImage(clientID,camara,0,vrep.simx_opmode_streaming)
-##time.sleep(1)
 #########
-def moverY():
-    x=0
-    y=0
-    posZ=0.5
-    while (x>-1.5):
-        y=-1.33*x
-        x=x-0.0075
-        vrep.simxSetObjectPosition(clientID,dron,-1,(0,y,posZ),vrep.simx_opmode_oneshot)
+
+def derecha():
+    moverY(1)
+
+def izquierda():
+    moverY(0)
+
+def arriba():
+    moverX(1)
+
+def abajo():
+    moverX(0)
+
+def zoomPositivo():
+    moverZ(1)
+
+def zoomNegativo():
+    moverZ(0)
+## contDesplamiento = numero de interacci?n dentro del while
+## tipo = 1 sumar ; 0 restar
+def moverY(tipo):
+    global posZ
+    global x
+    global y
+    global ts
+    global maxDesplamiento
+    cont = 0
+    while ( (y>=-4.70 and y<=2.25) and cont<=maxDesplamiento):
+        cont=cont+1
+        if tipo == 1:
+            y=y+0.1
+        elif tipo == 0:
+            y=y-0.1
+        vrep.simxSetObjectPosition(clientID,dron,-1,(x,y,posZ),vrep.simx_opmode_oneshot)
         time.sleep(ts)
 
-def moverX():
-    x=0
-    y=0
-    posZ=0.5
-    while (x>-1.5):
-        y=-1.33*x
-        x=x-0.0075
-        vrep.simxSetObjectPosition(clientID,dron,-1,(x,0,posZ),vrep.simx_opmode_oneshot)
+def moverX(tipo):
+    global posZ
+    global x
+    global y
+    global ts
+    global maxDesplamiento
+    cont = 0
+    while ( x>=-3.80 and x<=3.77 and cont<=maxDesplamiento):
+        cont=cont+1
+        if tipo == 1:
+            x=x-0.1
+        elif tipo == 0:
+            x=x+0.1
+
+        vrep.simxSetObjectPosition(clientID,dron,-1,(x,y,posZ),vrep.simx_opmode_oneshot)
         time.sleep(ts)
 
-def moverZ():
-    x=0
-    z=0.5
-    while (z<2):
-        z=z+0.005
-        vrep.simxSetObjectPosition(clientID,dron,-1,(x,0,z),vrep.simx_opmode_oneshot)
+def moverZ(tipo):
+    global posZ
+    global x
+    global y
+    global ts
+    global maxDesplamiento
+    cont = 0
+    while (posZ>0.50 and posZ<1.5):
+        cont=cont+1
+        if tipo == 1:
+            posZ=posZ+0.05
+        elif tipo == 0:
+            posZ=posZ-0.05
+
+        vrep.simxSetObjectPosition(clientID,dron,-1,(x,y,posZ),vrep.simx_opmode_oneshot)
         time.sleep(ts)
 
 def menu():
@@ -68,7 +108,12 @@ ventana = Frame(height=250,width=400)
 ventana.pack(padx=10,pady=10)
 etiqueta = Label(text="SIMULADOR DRON",font=("Verdana",18)).place(x=100,y=0)
 
-botonX = Button(ventana,command=moverY,text="<==").place(x=120,y=100)
-botonY = Button(ventana,command=moverX,text="==>").place(x=200,y=100)
-botonZ = Button(ventana,command=moverZ,text="Eje Z").place(x=160,y=65)
+botonXARR = Button(ventana,command=arriba,text="Arriba").place(x=165,y=65)
+botonYIZQ = Button(ventana,command=izquierda,text="Izquierda").place(x=120,y=100)
+botonYDER = Button(ventana,command=derecha,text="Derecha").place(x=200,y=100)
+botonXABA = Button(ventana,command=abajo,text="Abajo").place(x=165,y=135)
+botonZPOS = Button(ventana,command=zoomPositivo,text="Zoom +").place(x=20,y=82)
+botonZNEG = Button(ventana,command=zoomNegativo,text="Zoom -").place(x=20,y=112)
+
+
 ventana.mainloop()
